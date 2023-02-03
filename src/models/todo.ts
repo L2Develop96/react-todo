8e3c3ccfa4d45ebcd13b0247e9a7a778a9808a78
Hachem BEN AMOR <hachem.ben-amor@proxym-it.com>
2023-02-03 11:12:17 +0100

    fix: 🐛 Initial Commit

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,5 +1,3 @@
-export interface IToDo {
-  id: number;
-  name: string;
-  isComplete: boolean;
-}
+export {} from '';
+// Create interface called IToDo which contains the following properties:
+// id: number | name : string | isComplete : boolean
