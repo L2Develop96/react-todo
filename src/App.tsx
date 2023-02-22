644bffd2ff497cde76b761d61b606ae5b01ff16b
Hachem BEN AMOR <hachem.ben-amor@proxym-it.com>
2023-02-22 10:12:15 +0100

    fix: 🐛 Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import reactLogo from './assets/react.png';
 import './App.css';
+import { useRef } from 'react';
+
+// JS: Function Declaration & Function Expression
+
+// function aa () {
+
+// }
+// const app = function() {
+
+// }
 
 const App = () => {
+  const inputRef = useRef(null);
+
   return (
     <div className="container">
       <img className="logo" src={reactLogo} />
       <div>
-        <h2>Welcome aboard ♥ </h2>
+        <h2>TO DO APP</h2>
+        <div>
+          <input ref={inputRef} type="text" />
+          <button onClick={() => console.log(inputRef)}>ADD TODO</button>
+        </div>
       </div>
     </div>
   );
